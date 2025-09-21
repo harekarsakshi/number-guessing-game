db262263ece35bc0ebd90bd8fcfec5310b713943
sakshi-harekar <harekarsakshi@gmail.com>
2025-09-21 13:45:15 -0400

    fix: enhance game logic by adding attempt tracking and message updates

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ document.querySelector("#app").innerHTML = `
     <h1>Guess the Number (1-100)</h1>
     <input type="number" id="guessInput" placeholder="Enter your guess">
     <button id="guessBtn">Guess</button>
+    <h3 id="attempts"></h3>
     <p id="message"></p>
     <button id="resetBtn">Reset Game</button>
   </div>
@@ -17,7 +18,11 @@ const guessButton = document.getElementById("guessBtn");
 const message = document.getElementById("message");
 const guessInput = document.getElementById("guessInput");
 const resetButton = document.getElementById("resetBtn");
+const attempts = document.getElementById("attempts");
 var isGameInProgress = false;
+var attemptCount = 5;
+
+setAttempts();
 
 function generateRandom() {
   const n = Math.floor(Math.random() * 100) + 1;
@@ -25,16 +30,39 @@ function generateRandom() {
   return n;
 }
 
+function setMessage(text, color = "white") {
+  message.textContent = text;
+  message.style.color = color;
+}
+
+function setAttempts(count = attemptCount) {
+  attempts.textContent = `Attempts Left: ${count}`;
+}
+
 guessButton.addEventListener("click", () => {
+  if (attemptCount <= 1) {
+    setAttempts(0);
+    setMessage("No attempts left! Please reset the game.", "red");
+    return;
+  }
+
   const userGuess = Number(guessInput.value);
+  const isValidUserGuess = userGuess < 1 || userGuess > 100 || isNaN(userGuess);
+  if (isValidUserGuess) {
+    setMessage("Please enter a valid number between 1 and 100.", "red");
+    return;
+  }
+
   isGameInProgress = true;
+  attemptCount--;
+  setAttempts();
 
   if (userGuess === randomNumber) {
-    message.textContent = "Congratulations! You guessed it right!";
+    setMessage("Congratulations! You guessed it right!", "green");
   } else if (userGuess < randomNumber) {
-    message.textContent = "Too low! Try again.";
+    setMessage("Too low! Try again.");
   } else if (userGuess > randomNumber) {
-    message.textContent = "Too high! Try again.";
+    setMessage("Too high! Try again.");
   }
 });
 
